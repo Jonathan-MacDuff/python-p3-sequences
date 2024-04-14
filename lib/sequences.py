8e584ac2545c551703991a9fd9656ca3db786b3d
Jonathan MacDuff <jonathan.macduff@outlook.com>
2024-04-14 16:04:17 -0700

    completed lab

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,16 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fib = []
+    for i in range(length):
+        if i == 0:
+            fib.append(i)
+        elif i == 1:
+            fib.append(i)
+        else:
+            b = fib[-1]
+            c = fib[-2]
+            d = b + c
+            fib.append(d)
+        
+    print(fib)
